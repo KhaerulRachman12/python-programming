d95de939a83d2d92f9186b6dd54e2852aa7149f9
KhaerulRachman12 <khaerulrachman12@gmail.com>
2024-08-01 12:12:03 +0700

    Add files via upload

---
+++ pertemuan 4/penugasan.py
@@ -0,0 +1,25 @@
+x = 7
+
+#penjumlah
+x += 5
+print("penjumlah: ", x)
+
+#peengurangan
+x -= 2
+print("pengurangan: ", x)
+
+#perkalian
+x *= 7
+print("perkalian: ", x)
+
+#pembagian
+x /= 5
+print("pembagian: ", x)
+
+#modulus
+x %= 4
+print("modulus: ", x)
+
+#perpangkatan
+x **= 16
+print("pangkat: ", x)
