af87a68f0cb63308fcfcf948cfa146cd8fd0a7f1
KhaerulRachman12 <khaerulrachman12@gmail.com>
2024-08-29 12:33:30 +0700

    Add files via upload

---
+++ ujian/ujian.py
@@ -0,0 +1,22 @@
+jumlah siswa = int(input("masukan jumlah siswa: "))
+
+for i in range (jumlah siswa):
+    tugas_1 = float(input("masukan nilai tugas 1:"))
+    tugas_2 = float(input("masukan nilai tugas 2:"))
+    tugas_3 = float(input("masukan nilai tuga 3:"))
+    
+rata_rata = (nilai_tugas1 + nilai_tugas2 + nilai_tugas3 +)/3
+
+print("rata rata nilai tugas adalah:", rata_rata)
+
+if rata_rata >= 70:
+    print("lulus")
+    
+elif rata_rata <50:
+    print("tidak lulus")
+    
+elif rata_rata 50 and rata_rata < 69:
+    print("perbaiki lagi")
+    
+for i in range(jumlah siswa):
+    print(f"/n masukan nilai untuk siswa(i + 1):")
